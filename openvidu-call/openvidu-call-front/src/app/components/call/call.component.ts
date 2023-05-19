1eaf76c9e7b3d76148329e4eb44e748d87982cfe
jagdeep.khatana <khatanajagdeep1999@gmail.com>
2023-05-20 00:09:45 +0530

    Call Component html changes

--- openvidu-call/openvidu-call-front/src/app/components/call/call.component.ts
+++ openvidu-call/openvidu-call-front/src/app/components/call/call.component.ts
@@ -15,15 +15,15 @@ export class CallComponent implements OnInit {
 	joinSessionClicked: boolean = false;
 	closeClicked: boolean = false;
 	isSessionAlive: boolean = false;
-	recordingEnabled: boolean = true;
+	recordingEnabled1: boolean = true;
 	screenShareEnabled: boolean = true;
 	chatEnabled: boolean = true;
 	showSessionId:boolean=true;
 	broadcastingEnabled: boolean=true;
 	recordingList: RecordingInfo[] = [];
 	recordingError: any;
-	partitcipantName:string;
 	serverError: string = '';
+	participantName1:any
 	loading: boolean = true;
 	private isDebugSession: boolean = false;
 
@@ -63,6 +63,9 @@ export class CallComponent implements OnInit {
 		this.closeClicked = true;
 		this.router.navigate([`/`]);
 	}
+	participantName(){
+		return this.participantName1
+	}
 	async onStartRecordingClicked() {
 		try {
 			await this.restService.startRecording(this.sessionId);
@@ -94,7 +97,7 @@ export class CallComponent implements OnInit {
 			nickname = this.participantService.getLocalParticipant().getNickname();
 		}
 		const response = await this.restService.getTokens(this.sessionId, nickname);
-		this.recordingEnabled = response.isRecording
+		this.recordingEnabled1 = response.recordingEnabled
 		this.broadcastingEnabled=response.isBroadCasting
 		this.recordingList = response.recordings
 		this.tokens = {
@@ -104,7 +107,11 @@ export class CallComponent implements OnInit {
 		this.screenShareEnabled=response.isScreenSharing;
 		this.chatEnabled=response.isChatEnabled;
 		this.showSessionId=response.showSessionId;
-		this.partitcipantName=response.participantName;
-		console.log(this.partitcipantName);
+		this.participantName1=response.participantName;
+		console.log(response);
+		console.log(this.screenShareEnabled);
+	}
+	recordingEnabled(){
+		return this.recordingEnabled1;
 	}
 }
