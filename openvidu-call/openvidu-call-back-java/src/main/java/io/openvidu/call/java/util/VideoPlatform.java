289fb6fd797d1edcbd7923c3db0eac00ecb89b37
jagdeep.khatana <khatanajagdeep1999@gmail.com>
2023-05-22 13:07:18 +0530

    Http Client Deadlock fix

--- openvidu-call/openvidu-call-back-java/src/main/java/io/openvidu/call/java/util/VideoPlatform.java
+++ openvidu-call/openvidu-call-back-java/src/main/java/io/openvidu/call/java/util/VideoPlatform.java
@@ -77,7 +77,7 @@ public class VideoPlatform {
     this.httpClient = builder.build();
   }
 
-  public String getVideoPlatformProperties(String accountIdToken, String userIdToken,String sessionKey) {
+  public String getVideoPlatformProperties(String accountIdToken, String userIdToken,String sessionKey) throws IOException {
     HttpClientResponseHandler<String> responseHandler = new HttpClientResponseHandler<String>() {
       public String handleResponse(ClassicHttpResponse response) throws IOException, HttpException {
         int status = response.getCode();
@@ -100,6 +100,8 @@ public class VideoPlatform {
       return this.httpClient.execute(request, responseHandler);
     } catch (IOException e) {
       throw new RuntimeException(e.getMessage(), e.getCause());
+    }finally {
+      httpClient.close();
     }
   }
 
@@ -131,7 +133,7 @@ public class VideoPlatform {
     }
   }
 
-  public boolean sendSessionCallback(SessionCallback sessionCallback, int callbackRetryAttempts) {
+  public boolean sendSessionCallback(SessionCallback sessionCallback, int callbackRetryAttempts) throws IOException {
     StringEntity params = new StringEntity(sessionCallback.toString(), StandardCharsets.UTF_8);
     HttpPost request = new HttpPost(this.hostname + API_PATH + API_CALLBACK);
     request.setHeader(HttpHeaders.CONTENT_TYPE, "application/json");
@@ -173,6 +175,8 @@ public class VideoPlatform {
       logger.error("Error in calling callback for sessionId [{}]", sessionCallback.getUniqueSessionId(), e);
     } catch (Throwable t) {
       logger.error("Error in calling callback for sessionId [{}]", sessionCallback.getUniqueSessionId(), t);
+    }finally {
+      httpClient.close();
     }
     return true;
   }
