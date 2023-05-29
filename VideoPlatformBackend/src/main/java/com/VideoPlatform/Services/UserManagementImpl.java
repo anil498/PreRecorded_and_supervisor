910fc710bd1dea396d9c2f2ce08abdf16ada45bd
Vatsala Vats <vatsalavats18@gmail.com>
2023-05-29 14:02:10 +0530

    rename

---
+++ VideoPlatformBackend/src/main/java/com/VideoPlatform/Services/UserManagementImpl.java
@@ -0,0 +1,5 @@
+package com.VideoPlatform.Services;
+
+public class UserManagementImpl {
+    public static final String USER_UNAUTHORIZED = "user.unauthorized";
+}
--- VideoPlatformBackend/src/main/java/com/openvidu_databases/openvidu_dbbackend/Constant/AllConstants.java
+++
@@ -1,5 +0,0 @@
-package com.openvidu_databases.openvidu_dbbackend.Constant;
-
-public class AllConstants {
-    public static final Object SUBMISSION_STATE ="SUBMIT_FAILED";
-}
