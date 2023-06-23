e8c6cd65dbe02b5e73cb5c34f5655947607af2f6
prashant kumar <prashant.kumar@noi-dc01.mcarbon.com>
2023-06-23 14:14:10 +0530

    showing dynamic data on dashboard

---
+++ mcarbonPortal/src/app/model/dash_session.ts
@@ -0,0 +1,9 @@
+export interface DashSession {
+    totalSessions: number;
+    activeSessions: number;
+    expiredSessions: number;
+    dailySessionCreation: any;
+    monthlySessionCreation : any;
+    yearlySessionCreation : any;
+    isDisplay: boolean;
+}
---
+++ mcarbonPortal/src/app/model/dash_user.ts
@@ -0,0 +1,9 @@
+export interface DashUser {
+    totalUsers: number;
+    activeUsers: number;
+    expiredUsers: number;
+    dailyUserCreation: any;
+    monthlyUserCreation : any;
+    yearlyUserCreation : any;
+    isDisplay: boolean;
+}
