e3d60a015b685ef2edcda6149b749094855a020e
jagdeep.khatana <khatanajagdeep1999@gmail.com>
2023-06-29 13:21:42 +0530

    ios devices appear with blank video. Muting and unmuting it fix this problem

--- openvidu-call/openvidu-call-front/projects/openvidu-angular/src/lib/services/platform/platform.service.ts
+++ openvidu-call/openvidu-call-front/projects/openvidu-angular/src/lib/services/platform/platform.service.ts
@@ -22,7 +22,7 @@ export class PlatformService {
 	}
 
 	isIos(): boolean {
-		return this.isIPhoneOrIPad(navigator?.userAgent) || this.isSafariIos();
+		return this.isIPhoneOrIPad(navigator?.userAgent) || this.isIOSWithSafari(navigator?.userAgent);
 	}
 	private isIPhoneOrIPad(userAgent): boolean {
 		const isIPad = /\b(\w*Macintosh\w*)\b/.test(userAgent);
