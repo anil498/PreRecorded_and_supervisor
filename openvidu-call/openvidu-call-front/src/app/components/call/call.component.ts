62374babfbc231ef1a73d5fb2141a017f0225a4b
jagdeep.khatana <khatanajagdeep1999@gmail.com>
2023-07-24 17:43:07 +0530

    Supervisor bug fix

--- openvidu-call/openvidu-call-front/src/app/components/call/call.component.ts
+++ openvidu-call/openvidu-call-front/src/app/components/call/call.component.ts
@@ -259,13 +259,14 @@ export class CallComponent implements OnInit {
 				} else {
 					this.isVideoMuted = true;
 				}
-			} else {
-				this.isAudioMuted = false;
-				this.isVideoMuted = false;
-			}
-			if (response.settings.supervisor && this.type === 'Support') {
+			} else if(response.settings.supervisor) {
 				this.participantNameValue = 'Support';
 				this.addSupervisor = true;
+				this.isVideoMuted=false
+			}
+			else{
+				this.isAudioMuted = false;
+				this.isVideoMuted = false;
 			}
 		} catch (error) {
 			console.log(error);
